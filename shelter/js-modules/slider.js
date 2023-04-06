711bec46ac500ba05eb65a9bb167dbddb4f577bc
joyscript <joyscript7@gmail.com>
2023-04-07 02:57:13 +0300

    feat: improve main slider

--- shelter/js-modules/slider.js
+++ shelter/js-modules/slider.js
@@ -17,10 +17,14 @@ const makeSlider = (pets) => {
 
   let nums = [0, 1, 2, 3, 4, 5, 6, 7];
   let curArr = [[], [], []];
-  let storedArr = [[], [], []]; // This is for the immutability of cards on window resize
+  let forbidden = [];
 
   const addRandomCardsToSlide = (slide, pos, count) => {
-    let arr = nums.filter((num) => (pos === 1 ? !curArr.flat().includes(num) : ![...curArr[pos], ...curArr[1]].includes(num)));
+    let arr = nums.filter((num) => {
+      let exNums = pos === 1 || curCount === 1 ? [...curArr.flat()] : [...curArr[pos], ...curArr[1]];
+      if (pos !== 1) exNums.push(...forbidden);
+      return !exNums.includes(num);
+    });
 
     for (let i = 0; i < count; i++) {
       let ind = Math.floor(Math.random() * arr.length);
@@ -30,30 +34,39 @@ const makeSlider = (pets) => {
     }
   };
 
-  const copyArr = (arr) => [[...arr[0]], [...arr[1]], [...arr[2]]];
-
   const makeSlide = (pos) => {
     curArr[pos].length = 0;
     const slide = document.createElement('div');
     slide.className = 'slider__slide ';
     addRandomCardsToSlide(slide, pos, curCount);
-    storedArr = copyArr(curArr);
     return slide;
+  };
+
+  const removeLastCards = () => {
+    curArr = curArr.map((arr) => arr.slice(0, curCount));
+    [...sliderLine.children].forEach((slide) => {
+      [...slide.children].forEach((card, ind) => ind > curCount - 1 && card.remove());
+    });
   };
 
   const changeSlide = (e) => {
     slider.classList.remove('move-left', 'move-right');
 
+    if (curArr[0].length > curCount) removeLastCards();
+
     if (e.animationName === 'to-left') {
+      forbidden[0] = curArr[2][0];
       [[...curArr[1]], [...curArr[2]]] = [[...curArr[0]], [...curArr[1]]];
       sliderLine.prepend(makeSlide(0));
       sliderLine.lastElementChild.remove();
+      console.log(`left: [${curArr[1]}]`);
     } else {
+      forbidden[1] = curArr[0][0];
       [[...curArr[0]], [...curArr[1]]] = [[...curArr[1]], [...curArr[2]]];
       sliderLine.append(makeSlide(2));
       sliderLine.firstElementChild.remove();
+      console.log(`right: [${curArr[1]}]`);
     }
-    console.log(JSON.stringify(curArr), '---', JSON.stringify(storedArr));
   };
 
   const changeSliderOnResize = () => {
@@ -61,23 +74,16 @@ const makeSlider = (pets) => {
     curCount = getCardsCountPerPage();
     if (curCount === prevCount) return;
 
-    [...sliderLine.children].forEach((slide, pos) => {
-      if (curCount < prevCount) {
-        curArr[pos].pop();
-        slide.lastElementChild.remove();
-      } else if (curCount > prevCount && curCount > storedArr[pos].length) {
-        addRandomCardsToSlide(slide, pos, 1);
-        storedArr = copyArr(curArr);
-      } else {
-        curArr[pos] = [...storedArr[pos].slice(0, curCount)];
-        slide.append(generateCard(pets[curArr[pos][curCount - 1]]));
-      }
-    });
-    console.log(JSON.stringify(curArr), '---', JSON.stringify(storedArr));
+    if (curCount > prevCount && curCount > curArr[0].length) {
+      [...sliderLine.children].forEach((slide, pos) => {
+        addRandomCardsToSlide(slide, pos, curCount - prevCount);
+      });
+    }
+    console.log(`active: [${curArr[1].slice(0, curCount)}]`);
   };
 
   for (let i = 0; i < 3; i++) sliderLine.append(makeSlide(i));
-  console.log(JSON.stringify(curArr), '---', JSON.stringify(storedArr));
+  console.log(`active: [${curArr[1]}]`);
 
   prevBtn.addEventListener('click', () => slider.classList.add('move-left'));
   nextBtn.addEventListener('click', () => slider.classList.add('move-right'));
